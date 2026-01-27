36350135ab66d6e95ceb00133e365f5d8b11fa16
manishcicada <manishshar39@gmail.com>
2026-01-27 21:36:15 +0530

    added vercel.json

--- flask_app.py
+++ flask_app.py
@@ -617,4 +617,4 @@ def stream_video(kind: str, filename: str):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
